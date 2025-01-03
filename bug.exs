7921477c3e4f0fb722c9377a7cfc22db3b8128d0
BugHunter-X <askzenai@gmail.com>
2025-01-03 22:24:04 +0100

    Add bug.exs file

---
+++ bug.exs
@@ -0,0 +1,11 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+Enum.each(list, fn x ->
+  if x == 3 do
+    list = List.delete(list, x)  #Trying to modify the list in place
+  end
+  IO.puts(x)
+end)
+IO.inspect(list) 
+```
